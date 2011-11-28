8cd23569eb3b81d5170d150f83ca304cd9fc56a5
Ryan Crum <ryan.j.crum@gmail.com>
2011-11-28 16:08:11 -0500

    Fix typo in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject illusioniste "1.0.0-SNAPSHOT"
-  :description "A (very) wrapper over im4java, a wrapper over ImageMagick command-line utilities."
+  :description "A (very) thin wrapper over im4java, which is a wrapper over ImageMagick command-line utilities."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [im4java/im4java "1.1.0"]])
